8791fcc2bedcc10536da0bdaa121a84093dfaab5
Unknown <chinanet_mail@163.com>
2019-12-21 18:53:22 +0800

    add

---
+++ numpy-test.py
@@ -0,0 +1,26 @@
+import numpy
+
+data1 = [6, 7.5 , 8 , 0 , 1 ] 
+
+arr1 = numpy.array(data1)
+
+data2 = [[1,2,3,4] , [5,6,7,8]]
+
+arr2 = numpy.array(data2)
+
+print (arr2)
+
+print (arr2.ndim)
+
+print (arr2.shape)
+ 
+
+print( arr1.dtype ) 
+
+print( numpy.zeros(10) ) 
+
+print( numpy.zeros((3,6)) ) 
+
+print( numpy.zeros(( 2, 3, 2)) ) 
+
+print( numpy.arange(15) )
