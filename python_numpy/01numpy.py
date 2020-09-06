fba32a04bd681717b8a3f0cb40551442ba937632
Unknown <chinanet_mail@163.com>
2020-09-06 13:01:46 +0800

    多维

--- python_numpy/01numpy.py
+++ python_numpy/01numpy.py
@@ -5,12 +5,15 @@ import numpy as np
 
 """
 数组创建
-A 一维数组
-B 多维度数组
+A 一维数组，创建
+B 多维度数组，创建
 
 numpy.array(object, dtype = None, copy = True, order = None, subok = False, ndmin = 0)
 """
 
+"""
+一维数组
+"""
 # A1 base
 arr = np.array([1,2,3])
 
@@ -35,10 +38,15 @@ arr1 = np.array([
 print(arr1)
 print(arr1[1][1])
 
+
+"""
+多维数组
+"""
+
 # B2 最小维度
 arr1 = np.array([1,2,3,4,5,6] , ndmin = 1)
 arr2 = np.array([1,2,3,4,5,6] , ndmin = 2)
-
+print("  多维数组。。。。。。。。。。 ")
 print(arr1)
 print(arr2)
 
